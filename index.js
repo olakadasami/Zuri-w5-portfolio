96b1b030a3952f50c65e639a046d5acb4664f6d2
Okoye prosper <okoyeprosper475@gmail.com>
2022-10-27 07:31:20 +0100

    Added contact links

--- index.js
+++ index.js
@@ -3,31 +3,37 @@ const fs = require('fs')
 
 const server = http.createServer((req, res)=> {
     console.log("Server request made", req.method, req.url)
+    res.setHeader("Content-Type", "text/html")
     
     let path = './views/'
     switch (req.url) {
         case '/':
             path += 'home.html'
+            res.statusCode = 200
+            break;
+
+        case '/home':
+            res.statusCode = 301
+            res.setHeader("Location", '/')
+            res.end()
             break;
             
         case '/about':
             path += 'about.html'
+            res.statusCode = 200
             break;
         
         case '/contact':
             path += 'contact.html'
+            res.statusCode = 200
             break;
 
-        case '/home':
-            path += 'home.html'
-            break;
-        
         default:
-            path += 'home.html'
+            path += '404.html'
+            res.statusCode = 404
             break;
     }
 
-    res.setHeader("Content-Type", "text/html")
     fs.readFile(path, (err, data)=> {
         if(err) {
             console.log(err)
